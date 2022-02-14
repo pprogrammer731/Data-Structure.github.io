12691ca367ca73c488c377fddf4943478d7cede2
pprogrammer731 <94768931+pprogrammer731@users.noreply.github.com>
2022-02-14 19:09:51 +0300

    Add files via upload

---
+++ Assignment of singlyLinkedList/Q2.java
@@ -0,0 +1,17 @@
+
+/**
+ * Created by Mohammed Alnaimi on 5/2/2022.
+ */
+public class Q2 {
+//    public int mySize(){
+//        Node<E> n = head;
+//        int size = 0;
+//        if (head == null) return size;
+//        while (n != null){
+//            n = n.getNext();
+//            size++;
+//        }
+//        return size;
+//    }
+
+}
---
+++ Assignment of singlyLinkedList/Q3.java
@@ -0,0 +1,11 @@
+
+/**
+ * Created by Mohammed Alnaimi on 5/2/2022.
+ */
+public class Q3 {
+//    public SingleLinkedList<E> concat(SingleLinkedList<E> l1, SingleLinkedList<E> l2){
+//        l1.tail.setNext(l2.head);
+//        l1.size = l1.size()+l2.size();
+//        return l1;
+//    }
+}
